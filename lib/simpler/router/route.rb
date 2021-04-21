b5b365f982f811777404529e05bf2fcf6988017f
Andrii Boreiko <andriyboreyko@gmail.com>
2021-04-21 22:54:29 +0300

    upgrade project

--- lib/simpler/router/route.rb
+++ lib/simpler/router/route.rb
@@ -1,20 +1,29 @@
 module Simpler
   class Router
     class Route
-
-      attr_reader :controller, :action
+      attr_reader :controller, :action, :params
 
       def initialize(method, path, controller, action)
         @method = method
-        @path = path
+        @path_reg = get_reg_path(path)
         @controller = controller
         @action = action
+        @params = {}
       end
 
       def match?(method, path)
-        @method == method && path.match(@path)
+        path_match = path.match(@path_reg)
+        return false unless @method == method && path_match
+
+        @params = path_match.named_captures
+        true
       end
 
+      private
+
+      def get_reg_path(path)
+        Regexp.new "^#{path.gsub(/(:\w+)/, '(?<\1>\w+)').delete(':')}$"
+      end
     end
   end
 end
